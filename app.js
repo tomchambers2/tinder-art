056fe1d7ca890ef20f2b20a86bb8ddba37595c56
Tom Chambers <tom.chambers@gmail.com>
2015-04-11 13:04:08 +0100

    server and sockets running

---
+++ app.js
@@ -0,0 +1,11 @@
+var app = require('express')()
+var http = require('http').Server(app)
+require('./io')(http)
+
+app.get('/', function(req, res) {
+	res.sendfile('views/index.html')
+})
+
+http.listen(3000, function() {
+	console.log('now listening for server 3000')
+})
