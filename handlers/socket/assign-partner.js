b341b354a03aa13c8d682a7b1c7aaddf28e2c077
Tom Chambers <tom.chambers@gmail.com>
2015-06-13 11:54:28 +0100

    add character images to activated matches

--- handlers/socket/assign-partner.js
+++ handlers/socket/assign-partner.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var Tinder = require('../../lib/tinder')
+var characterFinder = require('../../lib/character-finder')
 
 module.exports = function(userId) {
 	var tinder = new Tinder(this.facebookUser.facebookUserId, this.facebookUser.token)
@@ -16,6 +17,12 @@ module.exports = function(userId) {
 				name: 'Unknown'
 			}
 		}
+
+		match.messages = match.messages.map(function(messageObj) {
+			messageObj.character = characterFinder(messageObj.message)
+			return messageObj
+		})
+		
 	    self.matchId = match._id
 	    self.partnerId = match.person._id
 	    self.emit('new partner', { photoUrl: match.person.photos && match.person.photos[0].url, name: match.person.name, matchId: self.matchId, partnerId: self.partnerId, messages: match.messages })		
